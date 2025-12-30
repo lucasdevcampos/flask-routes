9496c3f7f82cec43fc7f74e5aa5d5d6204b2bf67
Lucas Campos <lucasjcampos@hotmail.com>
2025-12-30 20:39:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return None
+    return render_template('home.html')
 
 @app.route('/user')
 def user():
-    return None
+    return redirect('https://github.com/lucasdevcampos')
 
 @app.route('/config')
 def config():
     return None
-
 
 if __name__ =='__main__':
     app.run(debug=True)
